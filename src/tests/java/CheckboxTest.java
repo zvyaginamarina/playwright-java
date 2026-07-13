082c5c98ece7a39311fb3408a0bf77f296b30ff3
mzviagina <admzv@yandex.ru>
2026-07-13 15:04:44 +0400

    Практическое задание №8

--- src/tests/java/CheckboxTest.java
+++ src/tests/java/CheckboxTest.java
@@ -34,11 +34,13 @@ public class CheckboxTest {
     private Locator checkBox1;
     private Locator checkBox2;
 
+    private static boolean HEADLESS = System.getenv("CI") != null;
+
     @BeforeAll
     @Step("Инициализация браузера")
     static void browserSetUp() {
         playwright = Playwright.create();
-        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(HEADLESS));
     }
 
     @BeforeEach
